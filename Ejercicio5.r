0f19579866f2e53c4f15610cd58d04905e3c3bc9
PabloSB <61882630+PabloSB@users.noreply.github.com>
2021-10-19 16:52:49 +0200

    Update Ejercicio5.r

--- Ejercicio5.r
+++ Ejercicio5.r
@@ -50,7 +50,7 @@ A %>%
 
 #Los puntos negros representan el percentil 85 del IMC para cada grupo de 
 #edad, mientras que los rojos, representan el percentil 95 para cada grupo.
-#La línea negra representa el nivel de IMC en el cual un adulto se considera
+#La línea negra representa el nivel de IMC en el cual, un adulto, se considera
 #que padece sobrepeso mientras que la rojo indica el nivel en el que un 
-#adulto padece obesidad.")
+#adulto padece obesidad."
 
