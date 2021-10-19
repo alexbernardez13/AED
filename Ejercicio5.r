278ec851852d9f37afe5a2a616fddc71cfdd9cb9
PabloSB <61882630+PabloSB@users.noreply.github.com>
2021-10-19 16:50:52 +0200

    Update Ejercicio5.r

--- Ejercicio5.r
+++ Ejercicio5.r
@@ -7,11 +7,11 @@ datos$IMC <- datos$peso / (datos$talla / 100)^2
 
 datos$Estado <- cut(datos$IMC,breaks = c(0,25,30,Inf), labels  = c("Normal","Sobrepeso","Obesidad"))
 
-tabla1 <- table( datos$Estado[datos$edad >= 18] )
-tabla2 <- prop.table(tabla1)
-tabla3 <- cbind(tabla1,tabla2)
-colnames(tabla3) <- c("Frecuencia Absoluta", "Frecuencia Relativa")
-tabla3
+Tabla1 <- table( datos$Estado[datos$edad >= 18] )
+Tabla2 <- prop.table(Tabla1)
+Tabla3 <- cbind(Tabla1,Tabla2)
+colnames(Tabla3) <- c("Frecuencia Absoluta", "Frecuencia Relativa")
+Tabla3
 
 
 #5.2 
@@ -39,104 +39,18 @@ A$rowname <- factor(A$rowname,levels =  c("(5-6]","(6-7]","(7-8]","(8-9]","(9-10
 
 A %>% 
   ggplot() +
-  geom_dumbbell(aes(x=Sobrepeso, xend=Obesidad, y=rowname),size_x = 3,size_xend = 3,colour_x = "black",colour_xend = "red") +
+  geom_dumbbell(aes(x=Sobrepeso, xend=Obesidad, y=rowname),size_x = 3,size_xend = 3,colour_x = "black",colour_xend = "red",show.legend = TRUE) +
   ylab("Rango de Edades")+
   xlab("IMC")+
   coord_flip()+
-  geom_vline(xintercept = 25,color ="black",size = 2 )+
-  geom_vline(xintercept = 30, color = "red",size = 2)+
-  expand_limits(x = c(15,30))
+  geom_vline(xintercept = 25,color ="black",size = 1.5 )+
+  geom_vline(xintercept = 30, color = "red",size = 1.5)+
+  expand_limits(x = c(15,30))+
+  theme_bw()
 
- 
-  
+#Los puntos negros representan el percentil 85 del IMC para cada grupo de 
+#edad, mientras que los rojos, representan el percentil 95 para cada grupo.
+#La línea negra representa el nivel de IMC en el cual un adulto se considera
+#que padece sobrepeso mientras que la rojo indica el nivel en el que un 
+#adulto padece obesidad.")
 
-
-
-
-
-
-
-
-
-################################################################################
-#                             BORRADORES                                       #
-################################################################################
-#          Se usó un loop para realizar la clasificación
-################################################################################
-datos$Estado <- 0
-
-for(i in 1:length(datos$IMC)) {  
-  if (datos$IMC[i] < 25) {
-    datos$Estado[i] <- "Normal"
-  } else { if (datos$IMC[i] >= 25 & (datos$IMC[i] < 30)) {
-    datos$Estado[i] <- "Sobrepeso"
-  } else {
-    datos$Estado[i] <- "Obesidad"
-  }
-  }
-}
-
-table(datos$Estado[datos$edad > 18])
-
-
-
-
-################################################################################
-datos$Categoría <- 0
-
-for(i in 1:length(datos$edad)) {
-  if (datos$edad[i] > 5 & datos$edad[i] <= 6) {
-    datos$Categoría[i] <- "5-6"
-  } else { if (datos$edad[i] > 6 & datos$edad[i] <= 7) {
-    datos$Categoría[i] <- "6-7"
-  } else { if (datos$edad[i] > 7 & datos$edad[i] <= 8) {
-    datos$Categoría[i] <- "7-8"
-  } else { if (datos$edad[i] > 8 & datos$edad[i] <= 9) {
-    datos$Categoría[i] <- "8-9"
-  } else { if (datos$edad[i] > 9 & datos$edad[i] <= 10){
-    datos$Categoría[i] <- "9-10"
-  } else { if (datos$edad[i] > 10 & datos$edad[i] <= 11) {
-    datos$Categoría[i] <- "10-11"
-  } else { if (datos$edad[i] > 11 & datos$edad[i] <= 12) {
-    datos$Categoría[i] <- "11-12"
-  } else { if (datos$edad[i] > 12 & datos$edad[i] <= 13) {
-    datos$Categoría[i] <- "12-13"
-  } else { if (datos$edad[i] > 13 & datos$edad[i] <= 14) {
-    datos$Categoría[i] <- "13-14"
-  } else { if (datos$edad[i] > 14 & datos$edad[i] <= 15) {
-    datos$Categoría[i] <- "14-15"
-  } else { if (datos$edad[i] > 15 & datos$edad[i] <= 16) {
-   datos$Categoría[i] <- "15-16"
-  } else { if (datos$edad[i] > 16 & datos$edad[i] <= 17) {
-   datos$Categoría[i] <- "16-17"
-  } else { if (datos$edad[i] > 17 & datos$edad[i] <= 18) {
-   datos$Categoría[i] <- "17-18"
-  } else { if (datos$edad[i] > 18 & datos$edad[i] <= 19) {
-   datos$Categoría[i] <- "18-19"
- }
- }
- }
- }
- }
- }
- }
- }
- }
- }
- }
- }
- }
- }  
- }
-
-?prop.table
-?addmargins
-
-
-dotchart(Tabla4$Sobrepeso,labels = row.names(Tabla4))
-points(Tabla4$Obesidad,1:row(Tabla4))
-
-
-
-dotchart(Tabla4$Sobrepeso, pch = 21, labels = row.names(Tabla4), bg = "green",pt.cex = 1.5, xlim = range(Tabla4$Sobrepeso, Tabla4$Obesidad) + c(-2, 2))
-points(Tabla4$Obesidad, 1:nrow(Tabla4), col = "red", pch = 19, cex = 1.5)
