0d869526148dc4f79e8e2e7d3c986fb5174abe46
PabloSB <61882630+PabloSB@users.noreply.github.com>
2021-11-03 15:04:36 +0100

    Create Ejercicio8.R

---
+++ Ejercicio8.R
@@ -0,0 +1,33 @@
+#Integral 1
+
+Integral <- function(f,a,b,n=NULL,alfa=0.05,conf.level=0.95,l=0.01) {
+  M <- max((seq(a,b)^2)) #Cota máxima
+  Q <- qnorm(1-alfa/2) #Qnorm
+  n <- ((M*(b-a)*Q)/l)^2 #Número óptimo con el intervalo 0.01
+  u <- runif(n,min=a,max=b) #Vector u[a,b]
+  v <- runif(n,min=0,max=M) #Vector v[0,M]
+  p <- mean(v <= u^2) # Proporción de valores de u menores que V
+  integral <- p*M*(1-0) #Aproximación integral
+  li= p*M*(b-a)-M*(b-a)*Q*sqrt(p*(1-p)/1000) #Límite inferior 
+  ls= p*M*(b-a)+M*(b-a)*Q*sqrt(p*(1-p)/1000) #Límite superior
+}
+
+
+# En integraln , nosotros fijamos el n
+Integraln <- function(f,a,b,n,alfa=0.05,conf.level=0.95,l=0.01) {
+  M <- max((seq(a,b)^2)) #Cota máxima
+  Q <- qnorm(1-alfa/2) #Qnorm
+  u <- runif(n,min=a,max=b) #Vector u[a,b]
+  v <- runif(n,min=0,max=M) #Vector v[0,M]
+  p <- mean(v <= u^2) # Proporción de valores de u menores que V
+  integral <- p*M*(1-0) #Aproximación integral
+  li= p*M*(b-a)-M*(b-a)*Q*sqrt(p*(1-p)/1000) #Límite inferior 
+  ls= p*M*(b-a)+M*(b-a)*Q*sqrt(p*(1-p)/1000) #Límite superior
+  print(integral)
+}
+
+
+Integraln(a=0,b=1,n=100) # n = 100
+Integraln(a=0,b=1,n=500) # n = 500
+Integraln(a=0,b=1,n=1000)# n = 1000
+Integraln(a=0,b=1,n=5000)# n = 5000
